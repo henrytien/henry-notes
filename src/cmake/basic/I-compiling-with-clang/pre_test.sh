b2b20ad814fa3d52a84d921734a50fd97a0dd789
henrytine <tianhangyu8886@gmail.com>
2020-04-23 11:05:29 +0800

    delete build.clang

--- src/cmake/basic/I-compiling-with-clang/pre_test.sh
+++ src/cmake/basic/I-compiling-with-clang/pre_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ROOT_DIR=`pwd`
-dir="01-basic/I-compiling-with-clang"
+#dir="01-basic/I-compiling-with-clang"
 
-if [ -d "$ROOT_DIR/$dir/build.clang" ]; then
+if [ -d "$ROOT_DIR/build.clang" ]; then
     echo "deleting $dir/build.clang"
-    rm -r $dir/build.clang
+    rm -r build.clang
 fi
